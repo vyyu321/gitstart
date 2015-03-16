4d01959ce71e84faad90084795f76e790d53cd94
root <root@localhost.localdomain>
2015-03-16 06:14:28 -0700

    wrote a readme file

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: ckl
+	> Mail:526299780@qq.com 
+	> Created Time: Mon 16 Mar 2015 06:11:46 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc,char**argv)
+{
+
+	return 0;
+}

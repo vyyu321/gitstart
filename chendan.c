d1611e4a8cf3667450fcb92858e28037c7fe8576
root <root@localhost.localdomain>
2015-03-16 07:39:25 -0700

    chendan

---
+++ chendan.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: chendan.c
+	> Author: ckl
+	> Mail:526299780@qq.com 
+	> Created Time: Mon 16 Mar 2015 07:37:04 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+int main(int argc,char**argv)
+{
+	printf("i love you chendan !\n");
+	return 0;
+}
